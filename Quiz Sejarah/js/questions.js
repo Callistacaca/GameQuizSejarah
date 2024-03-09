5f5e2fc2b8ab0efdf7984009d51db1d3fc901c0a
Callistacaca <132627542+Callistacaca@users.noreply.github.com>
2024-03-09 15:46:50 +0700

    Add files via upload

---
+++ Quiz Sejarah/js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Tujuan diselenggarakannya pemilu 1955 adalah untuk memilih anggota?",
+    answer: "DPR dan Badan Konstituante",
+    options: [
+      "DPR dan MPR",
+      "DPR dan Badan Konstituante",
+      "DPR tingkat I dan MPR",
+      "Badan Konstituante dan DPR Tingkat I"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Sebab utama jatuhnya Kabinet Sukiman adalahPada masa Demokrasi Terpimpin, selain mendapat dukungan kuat dari angkatan darat, Presiden juga mendapat dukungan penuh dari?",
+    answer: "Partai Komunis Indonesia",
+    options: [
+      "Partai Sosialis Indonesia",
+      "Partai Nasional Indonesia",
+      "Masyumi",
+      "Partai Komunis Indonesia"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Setelah adanya Dekrit Presiden 5 Juli 1959 sistem pemerintahan yang berlaku di Indonesia adalah?",
+    answer: "Demokrasi Terpimpin",
+    options: [
+      "Demokrasi Pancasila",
+      "Demokrasi Terpimpin",
+      "Demokrasi Gabungan",
+      "Demokrasi Liberal"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Pembubaran Uni Indonesia Belanda dilakukan saat Kabinet?",
+    answer: "Burhanuddin Harahap",
+    options: [
+      "Burhanuddin Harahap",
+      "Wilopo",
+      "Ali Sastroamidjojo II",
+      "Ali Sastroamidjojo I"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Tokoh ekonomi yang mempelopori program Gerakan Benteng ialah?",
+    answer: "Soemitro Djojohadikoesoemo",
+    options: [
+      "Kasman Singodimejo",
+      "Soemitro Djojohadikoesoemo",
+      "Kasimo",
+      "M. Hatta"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Penyebab jatuhnya Kabinet Wilopo adalah?",
+    answer: "Peristiwa Tanjung Morawa",
+    options: [
+      "Kegagalan perundingan dengan Belanda",
+      "Melanggar politik luar negeri bebas aktif",
+      "Peristiwa Tanjung Morawa",
+      "Kegagalan menyiapkan pemilu"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Berikut ini 'bukan' merupakan upaya untuk mengatasi krisis ekonomi pada masa Demokrasi Terpimpin adalah?",
+    answer: "Membentuk Dewan Nasional",
+    options: [
+      "Membentuk Komando Tertinggi Operasi Ekonomi (Kotoe)",
+      "Membentuk Deklarasi Ekonomi (Dekon)",
+      "Membentuk Dewan Nasional",
+      "Membentuk Kesatuan Operasi (Kesop)"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Politik yang menganggap bahwa negaranya yang paling baik dan hebat dibandingkan dengan negara yang lainnya disebut?",
+    answer: "Politik mercusuar",
+    options: [
+      "Politik mercusuar",
+      "Politik nasionalis",
+      "Politik terbuka",
+      "Politik bebas aktif"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Pemilu 1955 dan Dekrit 1959 merupakan momen penting dalam menguatkannya isu munculnya Demokrasi Terpimpin. Salah satu latar belakang munculnya Demokrasi Terpimpin adalah adanya kegagalan Konstituante dalam merumuskan?",
+    answer: "UUD Baru",
+    options: [
+      "UUD 1945",
+      "UUDS 1950",
+      "Konstitusi",
+      "UUD Baru"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Dalam rangka merebut kembali Irian Barat, Presiden Soekarno yang dirinya mengatas namakan penyambung lidah rakyat telah menyerukan komando rakyatnya yang dikenal dengan sebutan?",
+    answer: "Tri Komando Rakyat",
+    options: [
+      "Komando Gabungan Rakyat",
+      "Tri Komando Rakyat",
+      "Tri Tuntutan Rakyat",
+      "Dwi Komando Rakyat"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
